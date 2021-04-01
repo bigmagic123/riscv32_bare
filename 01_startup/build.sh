4f2d596f7c6f9774bf5485d870dbe95450b6f59d
bigmagic <fujin.zhao@foxmail.com>
2021-04-01 16:25:18 +0800

    add file

--- 01_startup/build.sh
+++ 01_startup/build.sh
@@ -1,3 +1,8 @@
-riscv-nuclei-elf-gcc -nostdlib -c entry.s -o entry.o
-riscv-nuclei-elf-gcc -nostdlib -c main.c -o main.o
-riscv-nuclei-elf-ld -o hello.elf -Tlink.ld entry.o main.o
+#riscv-nuclei-elf-gcc -O2 -g
+rm *.o *.o.d *elf -rf
+riscv-nuclei-elf-gcc -O2 -g -march=rv32imac -mabi=ilp32 -mcmodel=medany -ffunction-sections -fdata-sections -fno-common -DDOWNLOAD_MODE=DOWNLOAD_MODE_FLASHXIP -I. -MMD -MT entry.o -MF entry.o.d -c -o entry.o entry.S
+riscv-nuclei-elf-gcc -O2 -g -march=rv32imac -mabi=ilp32 -mcmodel=medany -ffunction-sections -fdata-sections -fno-common -DDOWNLOAD_MODE=DOWNLOAD_MODE_FLASHXIP -I. -MMD -MT intexc.o -MF intexc.o.d -c -o intexc.o intexc.S
+riscv-nuclei-elf-gcc -O2 -g -march=rv32imac -mabi=ilp32 -mcmodel=medany -ffunction-sections -fdata-sections -fno-common -DDOWNLOAD_MODE=DOWNLOAD_MODE_FLASHXIP -I. -MMD -MT main.o -MF mian.o.d -c -o main.o main.c
+riscv-nuclei-elf-ld -o hello.elf -Tlink.ld entry.o intexc.o main.o
+
+
--- 01_startup/main.c
+++ 01_startup/main.c
@@ -1,4 +1,53 @@
 //#include "sbi.h"
+
+/**
+ * \brief _init function called in __libc_init_array()
+ * \details
+ * This `__libc_init_array()` function is called during startup code,
+ * user need to implement this function, otherwise when link it will
+ * error init.c:(.text.__libc_init_array+0x26): undefined reference to `_init'
+ * \note
+ * Please use \ref _premain_init function now
+ */
+void __libc_init_array(void)
+{
+    /* Don't put any code here, please use _premain_init now */
+}
+
+void core_exception_handler(void)
+{
+    /* Don't put any code here, please use _premain_init now */
+}
+
+void SystemInit()
+{
+
+}
+
+void __libc_fini_array()
+{
+
+}
+
+void atexit()
+{
+
+}
+
+void _premain_init()
+{
+
+}
+
+// void _premain_init()
+// {
+
+// }
+void _postmain_fini()
+{
+
+}
+
 void main()
 {
 //   SBI_PUTCHAR('H');
